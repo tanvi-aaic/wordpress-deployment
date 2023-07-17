7234e492d01d5790bd09a637ba66f1563c1b24a0
Tanvi <129167954+tanvi-aaic@users.noreply.github.com>
2023-07-17 13:44:46 +0530

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -7,22 +7,22 @@ sudo apt update -y
 sudo apt install php libapache2-mod-php php-mysql php-redis php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip -y
 
 # Install Apache2 and MySQL
-sudo apt install apache2 mysql-server -y
+#sudo apt install apache2 mysql-server -y
 
 # Enable Apache2 and MySQL services
-sudo systemctl enable apache2 mysql
+#sudo systemctl enable apache2 mysql
 
 # Download and extract the latest WordPress
-wget -c http://wordpress.org/latest.tar.gz
-tar -xzvf latest.tar.gz
+#wget -c http://wordpress.org/latest.tar.gz
+#tar -xzvf latest.tar.gz
 
 # Move WordPress files to the appropriate directory
-sudo mkdir /var/www/wordpress
-sudo cp -R wordpress/* /var/www/wordpress
+#sudo mkdir /var/www/wordpress
+#sudo cp -R wordpress/* /var/www/wordpress
 
 # Set proper ownership and permissions
-sudo chown -R www-data:www-data /var/www/wordpress
-sudo chmod -R 775 /var/www/wordpress
+#sudo chown -R www-data:www-data /var/www/wordpress
+#sudo chmod -R 775 /var/www/wordpress
 
 # Log in to MySQL as root
-sudo mysql -u root
+#sudo mysql -u root
