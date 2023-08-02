2b90e2dd659dd63f699402437594a0a0a67d11c5
Tanvi <129167954+tanvi-aaic@users.noreply.github.com>
2023-08-02 12:34:09 +0530

    Create index.php

---
+++ wordpress-code/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello, World! This is a Website</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <p>This is a index.php file.</p>
+</body>
+</html>
